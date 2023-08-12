4b7058840e7812a065654b2c8fbbdff2a54b9814
sean craven <seanpietercraven@gmail.com>
2023-08-12 14:58:10 +0100

    chore: cleanup

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -1,4 +1,3 @@
--- since this is just an example spec, don't actually load anything here and return an empty spec
 return {
   { "mrjones2014/smart-splits.nvim" },
   {
@@ -28,10 +27,7 @@ return {
       opts.mapping = vim.tbl_extend("force", opts.mapping, {
         ["<Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
-            -- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
             cmp.confirm({ select = true })
-            -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-            -- this way you will only jump inside the snippet region
           elseif luasnip.expand_or_jumpable() then
             luasnip.expand_or_jump()
           elseif has_words_before() then
