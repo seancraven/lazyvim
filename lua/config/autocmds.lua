0dacd63cee9d36fe011ce114a02898b3962c819d
Sean Craven <sean.craven@advai.co.uk>
2024-02-03 08:55:51 +0000

    style(autocommands): extract local variabel

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,6 +1,7 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additio,nal autocmds here
+local programming_files = { "*.py", "*.sql", "*.md", "*.lua", "*.rs", "*.toml" }
 vim.api.nvim_create_autocmd("BufEnter", {
   pattern = "*.md",
   callback = function()
@@ -9,6 +10,6 @@ vim.api.nvim_create_autocmd("BufEnter", {
   end,
 })
 vim.api.nvim_create_autocmd("InsertEnter", {
-  pattern = { "*.py", "*.sql", "*.md", "*.lua", "*.rs", "*.toml" },
+  pattern = programming_files,
   callback = require("zen-mode").open,
 })
